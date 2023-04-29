c6980101cd13358ad00fd288bcb7c9a858edacd5
ManulMap <manul_map@cock.li>
2023-04-29 08:08:41 -0700

    add write_protected_memory function

--- include/reutl/win/memory.hh
+++ include/reutl/win/memory.hh
@@ -1,6 +1,9 @@
 #ifndef REUTL_MEMORY_HH
 #define REUTL_MEMORY_HH
 
+#include <expected>
+#include <type_traits>
+
 namespace reutl::win {
 
 enum class ErrCheckMemProtection { FailedVirtualQuery };
@@ -19,6 +22,28 @@ is_executable_addr(const void* addr) -> bool;
 
 [[nodiscard]] auto //
 is_guarded_addr(const void* addr) -> bool;
+
+enum class ErrWriteProtectedMem {
+    FailedGetOldProtect,
+    MemoryNotAllocated,
+    FailedChangeProtect,
+    FailedRestoreOldProtect
+};
+
+namespace detail {
+
+[[nodiscard]] auto write_protected_mem(void* dst, const void* src, std::size_t size)
+    -> std::expected<void, ErrWriteProtectedMem>;
+
+} // namespace detail
+
+template <typename T>
+    requires std::is_trivially_copyable_v<T>
+[[nodiscard]] auto //
+write_protected_mem(void* const dst, const T& src) -> std::expected<void, ErrWriteProtectedMem>
+{
+    return detail::write_protected_mem(dst, &src, sizeof(src));
+}
 
 } // namespace reutl::win
 
--- src/memory.cc
+++ src/memory.cc
@@ -2,6 +2,7 @@
 
 #include <Windows.h>
 #include <cassert>
+#include <cstring>
 
 namespace reutl::win {
 
@@ -57,5 +58,32 @@ is_guarded_addr(const void* const addr) -> bool
 
     return mbi.Protect & PAGE_GUARD;
 }
+
+namespace detail {
+
+[[nodiscard]] auto //
+write_protected_mem(void* const dst, const void* const src, const std::size_t size)
+    -> std::expected<void, ErrWriteProtectedMem>
+{
+    MEMORY_BASIC_INFORMATION mbi;
+    if (!VirtualQuery(dst, &mbi, sizeof(MEMORY_BASIC_INFORMATION)))
+        return std::unexpected(ErrWriteProtectedMem::FailedGetOldProtect);
+
+    if (mbi.Protect == PAGE_NOACCESS)
+        return std::unexpected(ErrWriteProtectedMem::MemoryNotAllocated);
+
+    DWORD temp_prot = PAGE_EXECUTE_READWRITE;
+    if (!VirtualProtect(dst, size, temp_prot, &mbi.Protect))
+        return std::unexpected(ErrWriteProtectedMem::FailedChangeProtect);
+
+    std::memcpy(dst, src, size);
+
+    if (!VirtualProtect(dst, size, mbi.Protect, &temp_prot))
+        return std::unexpected(ErrWriteProtectedMem::FailedRestoreOldProtect);
+
+    return std::expected<void, ErrWriteProtectedMem>{};
+}
+
+} // namespace detail
 
 } // namespace reutl::win
